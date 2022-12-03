93f997d303aa35d17bb1e49c40d64a74d716b84c
mano8 <eli.serra173@gmail.com>
2022-12-03 03:21:32 +0100

    Add some options to data tables demo

--- public/js/datatables-simple-demo.js
+++ public/js/datatables-simple-demo.js
@@ -1,9 +1,14 @@
+
+
 window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
+    const datatablesSimple = document.getElementById('tableSelec');
     if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple);
+        const datatable = new simpleDatatables.DataTable(datatablesSimple, {
+            perPageSelect: [5, 10, 15, 20, 25, 50, 100],
+            perPage: 25
+        });
     }
 });
